2581b64d5bd9384e6333b124ca3000ea26bfb79f
Frank Qi <dev@frankqi.ca>
2020-04-14 18:16:15 -0400

    Updated app install

--- mac/install_apps.sh
+++ mac/install_apps.sh
@@ -2,9 +2,14 @@
 
 ## Methods ##
 #############
+install_brew_app() {
+	if ! command -v $1 2>/dev/null >&2 ; then
+		brew install $1
+	fi
+}
 
 # $1 is the name of the brew cask application
-install() {
+install_cask_app() {
 	if ! brew cask list $1 2>/dev/null >&2 ; then
 		brew cask install $1
 	fi
@@ -17,16 +22,30 @@ install() {
 # Check if brew is already installed
 # POSIX use command -v <the_command>
 # Bash specific environments use hash <the_command> or type <the_command> to check builtin commands
-if ! hash brew 2>/dev/null; then
+if ! command -v brew 2>/dev/null; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
-install google-chrome
-install visual-studio-code
-install zoomus
-install webex-meetings
-install discord
-install rider
-install datagrip
-install xmind-zen
-install iterm2
+install_brew_app python3
+install_brew_app zsh
+## OhMyZsh ##
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+## Powerline Fonts ##
+git clone https://github.com/powerline/fonts.git --depth=1
+bash fonts/install.sh
+rm -rf fonts
+## Powerlevel9k Zsh Theme ##
+brew tap sambadevi/powerlevel9k
+brew install powerlevel9k
+
+install_cask_app datagrip
+install_cask_app discord
+install_cask_app google-chrome
+install_cask_app iterm2
+install_cask_app rider
+install_cask_app slack
+install_cask_app visual-studio-code
+install_cask_app webex-meetings
+install_cask_app xmind-zen
+install_cask_app zoomus
+
